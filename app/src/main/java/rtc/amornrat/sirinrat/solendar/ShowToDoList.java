3b7cddf28267d25330e732546d1e258d8f9629d1
sirinrat <tanlove.3010@gmail.com>
2016-01-14 16:25:43 +0700

    Show text view

--- app/src/main/java/rtc/amornrat/sirinrat/solendar/ShowToDoList.java
+++ app/src/main/java/rtc/amornrat/sirinrat/solendar/ShowToDoList.java
@@ -2,12 +2,36 @@ package rtc.amornrat.sirinrat.solendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ShowToDoList extends AppCompatActivity {
+    //Explicit
+    private TextView showDateTextView;
+    private ListView todoListView;
+    private String showDateString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_to_do_list);
+
+        //bind widget
+        bindWidget();
+       //show date
+        showDate();
+
+
+    }//Main Method
+
+    private void bindWidget() {
+        showDateTextView = (TextView) findViewById(R.id.txtShowDate);
+        todoListView = (ListView) findViewById(R.id.listView);
     }
-}
+
+    private void showDate() {
+        showDateString = getIntent().getStringExtra("Date");
+        showDateTextView.setText(showDateString);
+    }
+
+}//Main Class
